ed5776f6040947f12e31a09b29bf5022fe109962
ArtyRivera <54183353+ArtyRivera@users.noreply.github.com>
2021-03-16 12:03:20 -0400

    Added Fold toggle function to js

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@
 //Get the button
 var BtnTOP = document.getElementById("BtnScrollTOP");
 var BtnBTM = document.getElementById("BtnScrollBTM");
+var BtnFOLD = document.getElementById("BtnToggleFOLD")
 var ScrnHeight = document.documentElement.scrollHeight;
 
 // When the user clicks on the button, scroll to the top of the document
@@ -13,4 +14,17 @@ function FnScrollTOP() {
 function FnScrollBTM() {
     document.body.scrollTo(0,ScrnHeight); // For Safari
     document.documentElement.scrollTo(0,document.scrollHeight); // For Chrome, Firefox, IE and Opera
+}
+
+// Set display to "none" for element id:theFold if it is showing, and vice versa
+function ToggleFOLD() {
+    
+    var theFold = document.getElementById("theFold")
+
+    if (theFold.style.display === "none") {
+    theFold.style.display = "block";
+  } else {
+    theFold.style.display = "none";
+  }
+    
 }
